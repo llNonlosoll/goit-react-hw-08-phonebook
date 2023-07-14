bf45620644a8afb3084ead67fdb274e8ec0fa796
llNonlosoll <wishmasterar@gmail.com>
2023-07-14 22:26:15 +0200

    fix

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -1,47 +1,47 @@
-// import hooks useDispatch, useSelector
-import { useSelector, useDispatch } from 'react-redux';
-// import fetch function
-import { deleteContact } from 'redux/operations';
-// import selector
-import { selectFilteredContacts } from 'redux/selectors';
-// import styled components
-import {
-  ContactsListList,
-  ContactsListItem,
-  ContactsListItemContainer,
-  ContactsListButton,
-} from './ContactsList.styled';
+// // import hooks useDispatch, useSelector
+// import { useSelector, useDispatch } from 'react-redux';
+// // import fetch function
+// import { deleteContact } from 'redux/operations';
+// // import selector
+// import { selectFilteredContacts } from 'redux/selectors';
+// // import styled components
+// import {
+//   ContactsListList,
+//   ContactsListItem,
+//   ContactsListItemContainer,
+//   ContactsListButton,
+// } from './ContactsList.styled';
 
-// Contacts component
-export const Contacts = () => {
-  // getting a list of contacts from the state
-  const contacts = useSelector(selectFilteredContacts);
+// // Contacts component
+// export const Contacts = () => {
+//   // getting a list of contacts from the state
+//   const contacts = useSelector(selectFilteredContacts);
 
-  // variables dispatch
-  const dispatch = useDispatch();
+//   // variables dispatch
+//   const dispatch = useDispatch();
 
-  // delete contact function
-  const handleContactDelete = id => {
-    dispatch(deleteContact(id));
-  };
+//   // delete contact function
+//   const handleContactDelete = id => {
+//     dispatch(deleteContact(id));
+//   };
 
-  return (
-    <ContactsListList>
-      {contacts.map(contact => (
-        <ContactsListItem key={contact.id}>
-          <ContactsListItemContainer>
-            <p>{contact.name}: </p>
-            <p>{contact.phone}</p>
-          </ContactsListItemContainer>
-          <ContactsListButton
-            type="button"
-            name="delete"
-            onClick={() => handleContactDelete(contact.id)}
-          >
-            Delete
-          </ContactsListButton>
-        </ContactsListItem>
-      ))}
-    </ContactsListList>
-  );
-};
+//   return (
+//     <ContactsListList>
+//       {contacts.map(contact => (
+//         <ContactsListItem key={contact.id}>
+//           <ContactsListItemContainer>
+//             <p>{contact.name}: </p>
+//             <p>{contact.phone}</p>
+//           </ContactsListItemContainer>
+//           <ContactsListButton
+//             type="button"
+//             name="delete"
+//             onClick={() => handleContactDelete(contact.id)}
+//           >
+//             Delete
+//           </ContactsListButton>
+//         </ContactsListItem>
+//       ))}
+//     </ContactsListList>
+//   );
+// };
