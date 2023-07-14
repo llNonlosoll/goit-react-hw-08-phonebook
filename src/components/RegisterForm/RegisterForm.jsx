fd041cce801bd27b880d5751469e6e13f6f36b95
llNonlosoll <wishmasterar@gmail.com>
2023-07-14 20:29:54 +0200

    RegisterForm done

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,6 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { register } from 'redux/auth/operations';
+
 export const RegisterForm = () => {
+  const dispatch = useDispatch();
+
   const handleSubmit = event => {
     event.preventDefault();
+    const form = event.currentTarget;
+    dispatch(
+      register({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
   };
 
   return (
