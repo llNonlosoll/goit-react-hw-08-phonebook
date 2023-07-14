fef32207d64da4e2a93f506fb8a5e2615594328d
llNonlosoll <wishmasterar@gmail.com>
2023-07-14 20:41:06 +0200

    Login added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,13 +4,15 @@ import { Layout } from './Layout';
 
 const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route></Route>
       </Route>
     </Routes>
