618a8b765728911b66f7cdd000a5210a2beca3e4
llNonlosoll <wishmasterar@gmail.com>
2023-07-14 18:24:11 +0200

    HomePage, RegisterPage added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
+import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route></Route>
-        <Route></Route>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />}></Route>
         <Route></Route>
       </Route>
     </Routes>
---
+++ src/pages/Register.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet';
+// import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
+export default function Register() {
+  return (
+    <div>
+      <Helmet>
+        <title>Registration</title>
+      </Helmet>
+      <h3>RegisterForm</h3>
+      {/* <RegisterForm /> */}
+    </div>
+  );
+}
