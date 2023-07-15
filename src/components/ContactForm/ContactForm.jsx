29c8e2d24fdd22893096f7f8c2b247a40f281fd5
llNonlosoll <wishmasterar@gmail.com>
2023-07-15 09:11:47 +0200

    ContactForm done

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,59 @@
+import { useDispatch } from 'react-redux';
+import { useContacts } from 'hooks/useContacts';
+import { addContact } from 'redux/contacts/operations';
+
 export const ContactForm = () => {
-  return <h2>Hello</h2>;
+  const dispatch = useDispatch();
+
+  const { allContacts } = useContacts();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+
+    const isInContacts = allContacts.some(
+      ({ name }) =>
+        name.toLowerCase() === form.elements.name.value.toLowerCase()
+    );
+
+    if (isInContacts) {
+      alert(`${form.elements.name.value} is already in contacts`);
+      return;
+    }
+
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+
+    dispatch(addContact({ name, number }));
+
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">
+        Name
+        <input
+          id="name"
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </label>
+      <label htmlFor="number">
+        Number
+        <input
+          id="number"
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </label>
+      <button type="submit">Add Contact</button>
+    </form>
+  );
 };
