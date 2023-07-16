9f60b542013fef68672c228f13d7a8263fde7693
llNonlosoll <117765352+llNonlosoll@users.noreply.github.com>
2023-07-16 08:25:16 +0000

    Deploying to gh-pages from @ llNonlosoll/goit-react-hw-08-phonebook@b5b2c102e060bee124c7c1904840504a0e8835aa 🚀

---
+++ static/js/415.b85ba503.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Welcome to Phonebook! "})})}}}]);
+//# sourceMappingURL=415.b85ba503.chunk.js.map
